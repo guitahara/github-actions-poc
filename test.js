9d27fc6967d11d470bf5000b74733dd4c02d79fd
Guilherme Tahara Benedeti <guilhermetb1@gmail.com>
2020-03-28 19:15:37 -0300

    inicio do projeto

---
+++ test.js
@@ -0,0 +1,14 @@
+const {expect} = require('chai')
+const {add, subtract} = require('./index')
+
+describe('Testes', function() {
+    it('Sucesso: adição de números', () => {
+        const result = add(1,1)
+        expect(result).to.be.eql(2)
+    }) 
+
+    it('Sucesso: subtração de números', () => {
+        const result = subtract(1,1)
+        expect(result).to.be.eql(0)
+    }) 
+})
